da4eff26d3adb10dfc9c3bbe653e51c7551b0383
BL-CoderOvertime <brandonlively@live.com>
2019-06-21 14:12:55 -0400

    setup delete book endpoint

--- src/main/java/com/lambdaschool/starthere/controllers/BookStoreController.java
+++ src/main/java/com/lambdaschool/starthere/controllers/BookStoreController.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -44,5 +46,20 @@ public class BookStoreController {
         List<Book> allBooks = bookService.findAll();
         return new ResponseEntity<>(allBooks, HttpStatus.OK);
     }
+
+    // - /data/books/{id}
+
+
+    //DELETE /data/books/{id} - deletes a book and the book author combinations - but does not delete the author records.
+
+    @DeleteMapping(value = "/data/books/{id}")
+    public ResponseEntity<?> deleteBookById(HttpServletRequest request, @PathVariable long id)
+    {
+        logger.trace(request.getRequestURI() + " accessed");
+
+        bookService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
 }
