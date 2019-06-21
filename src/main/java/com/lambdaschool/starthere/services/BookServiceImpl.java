ec5a02162d84ef16eebcf0679c4b0bbf8e13132e
BL-CoderOvertime <brandonlively@live.com>
2019-06-21 13:06:50 -0400

    Setup BookService and BookServiceImpl

---
+++ src/main/java/com/lambdaschool/starthere/services/BookServiceImpl.java
@@ -0,0 +1,40 @@
+package com.lambdaschool.starthere.services;
+
+import com.lambdaschool.starthere.models.Book;
+import com.lambdaschool.starthere.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookServiceImpl implements BookService{
+
+    @Autowired
+    BookRepository bookrepos;
+
+
+    @Override
+    public List<Book> findAll() {
+        List<Book> list = new ArrayList<>();
+        bookrepos.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+    @Override
+    public Book findBookById(long id) {
+        return bookrepos.findById(id).orElseThrow(() -> new EntityNotFoundException(Long.toString(id)));
+    }
+
+    @Override
+    public void delete(long id) {
+        bookrepos.deleteById(id);
+    }
+
+    @Transactional
+    @Override
+    public Book save(Book book) {
+        return bookrepos.save(book);
+    }
+}
