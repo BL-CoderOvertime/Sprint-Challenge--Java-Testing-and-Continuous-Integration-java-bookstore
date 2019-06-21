650d8236ff0eaaab8bb67cbc63761a024d38f893
BL-CoderOvertime <brandonlively@live.com>
2019-06-21 11:39:45 -0400

    Initial Commit - Added Start Here

---
+++ src/main/java/com/lambdaschool/starthere/StarthereApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.starthere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarthereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarthereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lambdaschool/starthere/StarthereApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.starthere;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StarthereApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
